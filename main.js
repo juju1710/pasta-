489b8cc711a3fa79f986fa88e25e2e04c3853255
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-03-25 08:36:53 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const botoes =  document ; querySelectorAll ("botao")
+const textos =  document ; querySelectorAll (".aba-conteudo")
+
 for(let i=0 ; i < botoes.length ; i ++){
     botoes.onclick = function () {
         for( let j = 0 ; j < botoes.length ; j ++ ){
-            botoes.classlist.remove( "ativo");
+            botoes[j].classlist.remove( "ativo");
+            textos[i].classlist.remove( "ativo");
         }
-botoes[i]. classlist. add ("ativo");
-        
+        botoes[i]. classlist. add ("ativo");
+        textos[i]. classlist. add ("ativo");
     }
 }
