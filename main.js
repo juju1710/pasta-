62db63a3b1b6512dd5658290f04fcea3d0094142
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-03-27 09:19:14 -0300

    .

--- main.js
+++ main.js
@@ -14,4 +14,17 @@ for(let i=0 ; i < botoes.length ; i ++){
 
 const cotadores = document.querySelectorAll(".contador");
 const tempoObjetivo1= new date ("2024-03-25T00:00:00");
-let tempoAtual = new date ();
+
+contadores [0]. textContent = calculaTempo ( tempoObjetivo1) ;
+ function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new date ();
+    let tempoFinal= tempoObjetivo- tempoAtual;
+    let segundos = math.floor(tempoFinal/100);
+    let minutos = math.floor(segundos/60);
+    let horas = math.floor(miutos/60);
+    let dias = math.floor(horas/24);
+    segundos % = 60;
+    minutos % =60;
+    horas % = 24;
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos"
+}
